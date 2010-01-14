9d151e48cf97f8f80928ad26d0b6fe76b5d118af
Claudio Cannata <cyberpro4@gmail.com>
2010-01-14 23:40:26 +0100

    scopiazzato dall esempio di qt

--- ccodeeditor.h
+++ ccodeeditor.h
@@ -1,15 +1,52 @@
 #ifndef CCODEEDITOR_H
 #define CCODEEDITOR_H
 
-#include <QtGui/QWidget>
+#include <QtGui>
 
-class CCodeEditor : public QWidget
+class LineNumberArea;
+
+class CCodeEditor : public QPlainTextEdit
 {
     Q_OBJECT
 
 public:
     CCodeEditor(QWidget *parent = 0);
     ~CCodeEditor();
+
+
+    void lineNumberAreaPaintEvent(QPaintEvent *event);
+    int lineNumberAreaWidth();
+
+protected:
+    void resizeEvent(QResizeEvent *event);
+
+private slots:
+    void updateLineNumberAreaWidth(int newBlockCount);
+    void highlightCurrentLine();
+    void updateLineNumberArea(const QRect &, int);
+
+private:
+    QWidget *lineNumberArea;
+};
+
+class LineNumberArea : public QWidget
+{
+public:
+    LineNumberArea(QWidget *editor) : QWidget(editor) {
+	codeEditor = (CCodeEditor*)editor;
+    }
+
+    QSize sizeHint() const {
+	return QSize(codeEditor->lineNumberAreaWidth(), 0);
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event) {
+	codeEditor->lineNumberAreaPaintEvent(event);
+    }
+
+private:
+    CCodeEditor *codeEditor;
 };
 
 #endif // CCODEEDITOR_H
