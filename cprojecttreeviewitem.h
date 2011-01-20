7c627173d6f55eafd3861bda156cb7ada33ef92d
Claudio Cannata <c.cannata@imts.eu>
2011-01-20 09:16:16 +0100

    Add to each file GPLv3 LICENSE

--- cprojecttreeviewitem.h
+++ cprojecttreeviewitem.h
@@ -1,3 +1,24 @@
+/**************************************************************************
+
+    Copyright (C) 2011 Claudio Cannatà.
+
+    This file is part of Magnum.
+
+    Magnum is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Magnum is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Magnum.  If not, see <http://www.gnu.org/licenses/>.
+
+**************************************************************************/
+
 #ifndef CPROJECTTREEVIEWITEM_H
 #define CPROJECTTREEVIEWITEM_H
 
